6f092d31c579353b0072ec2b6f879d1dd655bda3
maon-maon <fhud9839@gmail.com>
2024-11-12 09:29:46 +0900

    20241111

---
+++ src/main/java/study2/database/DbTestDAO.java
@@ -0,0 +1,12 @@
+package study2.database;
+
+import java.util.ArrayList;
+
+public class DbTestDAO {
+
+	public ArrayList<DbTestVO> getDbTestList(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/study2/database/DbTestVO.java
@@ -0,0 +1,5 @@
+package study2.database;
+
+public class DbTestVO {
+
+}
